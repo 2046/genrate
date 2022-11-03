0c20b60504932b766e732df1db1b9576db6853ca
2046 <g@ggg.gd>
2022-11-03 22:42:51 +0800

    wip: unzip util done

--- src/commands/use.ts
+++ src/commands/use.ts
@@ -1,8 +1,15 @@
-import { npm, download, TMP_PATH } from '../utils'
+import { join } from 'path'
+import { npm, download, unzip, TMP_PATH, TEMPLATE_PATH } from '../utils'
 
 export default async function use(template: string) {
   const url = await npm.getCompressedPackageUrl(template, 'latest')
-  const filePath = await download(url, TMP_PATH)
+  const tmpPath = await download(url, TMP_PATH)
+  const templatePath = await unzip(tmpPath, join(TEMPLATE_PATH, template), { override: true })
 
-  console.log(filePath)
+  console.log({
+    template,
+    url,
+    tmpPath,
+    templatePath
+  })
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,14 @@
 import npm from './npm'
 import axios from 'axios'
+import rimraf from 'rimraf'
+import { promisify } from 'util'
+import decompress from 'decompress'
+import { lstatSync } from 'fs-extra'
 import { basename, join } from 'path'
-import { TMP_PATH } from './constants'
 import { createWriteStream } from 'fs'
+import { TMP_PATH, TEMPLATE_PATH } from './constants'
 
-export { npm, TMP_PATH }
+export { npm, TMP_PATH, TEMPLATE_PATH }
 
 export function download(url: string, dest: string) {
   return new Promise<string>((resolve, reject) => {
@@ -17,4 +21,14 @@ export function download(url: string, dest: string) {
         .on('error', (error) => reject(error))
     })
   })
+}
+
+export async function unzip(filePath: string, dest: string, options: { override: boolean } = { override: false }) {
+  if (options.override && lstatSync(dest).isDirectory()) {
+    await promisify(rimraf)(dest)
+  }
+
+  await decompress(filePath, dest, { strip: 1 })
+
+  return dest
 }
