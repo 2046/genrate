b13348855c0cdc5852cbf36d648f921fb102c464
2046 <g@ggg.gd>
2022-11-03 23:28:45 +0800

    feat: list command done

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,3 +1,21 @@
-export default function list() {
-  // empty
+import { join } from 'path'
+import { readdir } from 'fs-extra'
+import { npm, TEMPLATE_PATH } from '../utils'
+
+export default async function list() {
+  const templates = await readdir(TEMPLATE_PATH)
+
+  const tree = templates.length
+    ? [
+        TEMPLATE_PATH,
+        ...templates.map((template, index) => {
+          const branch = index === templates.length - 1 ? '└── ' : '├── '
+          const version = npm.readPackageJson(join(TEMPLATE_PATH, template, 'package.json')).version
+
+          return `${branch}${template}@${version}`
+        })
+      ]
+    : [TEMPLATE_PATH, '└── (empty)']
+
+  console.log(tree.join('\n'))
 }
