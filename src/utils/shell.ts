0f7a5976b921b74440cde8774118d4340ea54279
2046 <g@ggg.gd>
2022-11-16 23:46:43 +0800

    wip: exec shell support cwd option

--- src/utils/shell.ts
+++ src/utils/shell.ts
@@ -1,5 +1,23 @@
 import shelljs from 'shelljs'
+import cp, { ExecException } from 'child_process'
 
-export function exec(command: string, silent = true) {
-  return shelljs.exec(command, { silent })
+export function exec(
+  command: string,
+  options?: {
+    cwd?: string
+    silent?: boolean
+  }
+) {
+  const { cwd = '', silent = true } = options || {}
+
+  if (cwd) {
+    try {
+      const result = cp.execSync(command, { cwd, stdio: silent ? 'ignore' : undefined })
+      return { code: 0, stdout: result.toString(), stderr: '' }
+    } catch (error) {
+      return { code: 1, stdout: '', stderr: (error as ExecException).message }
+    }
+  } else {
+    return shelljs.exec(command, { silent })
+  }
 }
