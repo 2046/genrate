0f4c0b2a0a30ff1573e805f33bf3281f7cc0d9ea
2046 <g@ggg.gd>
2022-11-24 22:12:10 +0800

    feat: update npmignore

--- src/templates/ignore/npm.ts
+++ src/templates/ignore/npm.ts
@@ -1,3 +1,24 @@
-export default `# -- Husky config --
+export default `# -- Lint config --
 .husky
+.lintstagedrc
+
+# -- Commit lint config --
+.czrc
+.cz-config.js
+.commitlintrc
+
+# -- ESlint config --
+.eslintrc
+
+# -- Prettier config --
+.prettierrc
+
+# -- Build config --
+rollup.config.js
+babel.config.json
+
+# -- test config --
+/test
+/coverage
+jest.config.json
 `
